9f059b93aa361789a18d4bf079f526415fe13703
kartik <karrv95@gmail.com>
2021-09-23 13:13:04 -0400

    Created Data in Database. Added 2 lines for Media handling. Edited main project url files to include the media. Changed Models in travello to handle images and upload to pics folder in media

--- kv_django/travello/views.py
+++ kv_django/travello/views.py
@@ -3,26 +3,5 @@ from .models import Destination
 
 # Create your views here.
 def index(request):
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.desc = 'The city of Dreams'
-    dest1.img = 'destination_1.jpg'
-    dest1.price = 700
-    dest1.offer = True
-
-    dest2 = Destination()
-    dest2.name = 'Banglore'
-    dest2.desc = 'The city of Traffic'
-    dest2.img = 'destination_5.jpg'
-    dest2.price = 800
-    dest2.offer = False
-
-    dest3 = Destination()
-    dest3.name = 'Gujrat'
-    dest3.desc = 'The city of Factories'
-    dest3.img = 'destination_9.jpg'
-    dest3.price = 500
-    dest3.offer = False
-
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
     return render(request, 'index.html', {'dests': dests})
