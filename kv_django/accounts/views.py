b68c760de5f5dca0ac68954a2d6fed71b8ba6f55
kartik <karrv95@gmail.com>
2021-09-23 14:07:00 -0400

    Display messages on error. changes in views and register html files for accounts

--- kv_django/accounts/views.py
+++ kv_django/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
+from django.contrib import messages
 # Create your views here.
 
 def register(request):
@@ -14,15 +15,18 @@ def register(request):
 
         if password1==password2:
             if User.objects.filter(username=username).exists():
-                print('User name already exists!')
+                messages.info(request, 'Username exists!')
+                return redirect('register.html')
             elif User.objects.filter(email=email).exists():
-                print('Email already exists!')
+                messages.info(request, 'Email already exists!')
+                return redirect('register.html')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('USER CREATED')
         else:
             print('Password Not Matching!')
+            return redirect('register.html')
         return redirect('/')
 
     else:
