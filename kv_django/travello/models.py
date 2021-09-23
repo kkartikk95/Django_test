d64b3fa2206e04aaab1458c613cb4d6f5ef4e933
kartik <karrv95@gmail.com>
2021-09-23 12:05:40 -0400

    Missing paranthesis in models file near price

--- kv_django/travello/models.py
+++ kv_django/travello/models.py
@@ -6,6 +6,6 @@ class Destination(models.Model):
     name = models.CharField(max_length=200)
     img = models.ImageField(upload_to='pics')
     desc = models.TextField()
-    price = models.IntegerField
+    price = models.IntegerField()
     offer = models.BooleanField(default=False)
 
